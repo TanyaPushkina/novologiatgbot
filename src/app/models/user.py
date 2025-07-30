4bcad7ebadd8b76ba3945550e03d4955ff218e2a
Tanya <taniushapushkina@gmail.com>
2025-07-30 18:19:02 +0300

    changeditmodels

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer, String
-from app.core.session import Base  
+from sqlalchemy import Integer, String, Float  # ← обязательно
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.session import Base
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True, nullable=False)
-    name = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String, nullable=False)
