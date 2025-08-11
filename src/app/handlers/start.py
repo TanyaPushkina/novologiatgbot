f5e92bf517fc8ab0cc79f1857cb4c0fab5320cd9
Tanya <taniushapushkina@gmail.com>
2025-08-11 21:57:04 +0300

    validation

--- src/app/handlers/start.py
+++ src/app/handlers/start.py
@@ -1,22 +1,4 @@
-'''from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.fsm.context import FSMContext  
 
-from app.keyboards import MainMenuKeyboard
-
-class StartHandler:
-    def __init__(self):
-        self.router = Router()
-        self.router.message(CommandStart())(self.start)
-
-    async def start(self, message: Message, state: FSMContext) -> None:  
-        await state.clear()  
-        await message.answer(
-            "Привет, это бот школы Новология!\n"
-            "Я помогу вам выбрать курс и записаться.",
-            reply_markup=MainMenuKeyboard.get()
-        )'''
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
